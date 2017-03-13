e86a97a78ec9ebe39313a0f89811edd81fc188aa
tamamu <tamamu.1r1s@gmail.com>
2017-03-14 00:49:55 +0900

    Corresponded to new API

--- t/glisph.lisp
+++ t/glisph.lisp
@@ -62,26 +62,37 @@
         *height* height))
 
 (defmethod glut:display-window :before ((w test-window))
+  (gli:init *width* *height*)
+  (pass "Success: init")
   (setf *font-en* (gli:open-font-loader
                    (merge-pathnames "Ubuntu-R.ttf" *load-truename*))
         *font-ja* (gli:open-font-loader
                    (merge-pathnames "mplus-1m-regular.ttf" *load-truename*)))
   (setf *glyph-table-en* (gli:make-glyph-table *font-en*)
         *glyph-table-ja* (gli:make-glyph-table *font-ja*))
+  (pass "Success: make-glyph-table")
   (loop for text across *text-en*
         do (gli:regist-glyphs *glyph-table-en* text))
   (loop for text across *text-ja*
         do (gli:regist-glyphs *glyph-table-ja* text))
+  (pass "Success: regist-glyphs")
   (setf *text-en*
-        (map 'vector
-             (lambda (text) (gli:new-vstring *glyph-table-en* text 0.0))
-             *text-en*))
+        (gli:draw *glyph-table-en*
+          '(:size 20 :x 0 :y 0
+           :text (aref *text-en* 0)
+           :size 30 :x 0 :y 24
+           :text (aref *text-en* 1)
+           :size 40 :x 0 :y 60
+           :text (aref *text-en* 2))))
   (setf *text-ja*
-        (map 'vector
-             (lambda (text) (gli:new-vstring *glyph-table-ja* text 0.0))
-             *text-ja*))
-  (format t "Success: new-vstring~%")
-  (ok (gli:init)))
+        (gli:draw *glyph-table-ja*
+          `(:size 24 :x 200
+            ,@(loop for idx from 0 below (length *text-ja*)
+                  append (list :spacing (* idx 4)
+                               :y (+ 200 (* idx 60))
+                               :text (aref *text-ja* idx))))))
+  (pass "Success: draw")
+  (ok t))
 
 (defmethod glut:tick ((w test-window))
   (incf *frame-count*)
@@ -102,33 +113,17 @@
     (gl:vertex 0 300))
   (let* ((rad (* (coerce pi 'single-float) (/ *frame-count* 180)))
          (cosr (cos rad)))
-    (gli:with-context *width* *height*
-      (gli:set-rotate 0.0 0.0 0.0)
-      (gli:set-table *glyph-table-en*)
-      (gli:set-color 0 0 0)
-      (gli:draw
-       '(:x 72 :y 150 :size 32 "Hello world!"
-         :x 24 :y 300 :size 12 "GLisph"))
-      (gli:draw
-       `(:size 32
-         ,@(loop for i from 0 below (length ,*text-en*)
-                 collect :x
-                 collect (- ,*display-x* 300.0)
-                 collect :y
-                 collect (+ ,*display-y* (- (* i 32.0) 150.0))
-                 collect (aref ,*text-en* i))))
-      (gli:set-rotate 0.0 0.0 rad)
-      (gli:set-table *glyph-table-ja*)
-      (gli:set-color 1.0 1.0 1.0)
-      (gli:draw
-       `(:size 24
-         ,@(loop for i from 0 below (length ,*text-ja*)
-                 collect :x
-                 collect (+ *display-x*
-                            (* -300.0 (+ ,cosr (* i 0.25))))
-                 collect :y
-                 collect (+ *display-y* (* i 32.0)))))
-      (gl:flush))
+
+    ;; Currently, because of #2, it has no effect in this context.
+    (gli:gcolor 1.0 1.0 1.0 1.0)
+
+    (gli:render *text-en*)
+
+    ;; #2 It overwrites the color drawn before.
+    (gli:gcolor cosr 1.0 1.0 1.0)
+
+    (gli:render *text-ja*)
+    (gl:flush)))
 
 (defmethod glut:close ((w test-window))
   (gli:delete-glyph-table *glyph-table-en*)
@@ -136,7 +131,7 @@
   (gli:finalize)
   (format t "close~%"))
 
-(plan 1)
+(plan 5)
 
 (glut:display-window (make-instance 'test-window))
 
